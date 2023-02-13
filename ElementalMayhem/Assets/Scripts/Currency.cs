8838d15d58d1615c3f57ccd1c261dae07c01ce02
WTuneSeek <91195791+WTuneSeek@users.noreply.github.com>
2023-02-13 08:46:51 +0100

    begin markt

---
+++ ElementalMayhem/Assets/Scripts/Currency.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Currency : MonoBehaviour
+{
+
+
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.GetComponent<PlayerStats>().goldCoin += 1;
+        }
+    }
+}
